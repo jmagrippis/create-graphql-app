65da558c24c57d49b64883fd5c3e92a3d9934223
Johnny Magrippis <j@magrippis.com>
2019-04-16 18:38:05 +0100

    add a lightweight web app as the default service

---
+++ web/src/types.ts
@@ -0,0 +1,24 @@
+type Maybe<T> = T | null
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string
+  String: string
+  Boolean: boolean
+  Int: number
+  Float: number
+  /** The `Upload` scalar type represents a file upload. */
+  Upload: any
+}
+
+export enum CacheControlScope {
+  Public = 'PUBLIC',
+  Private = 'PRIVATE'
+}
+
+export type Query = {
+  greeting: Scalars['String']
+}
+
+export type QueryGreetingArgs = {
+  name?: Maybe<Scalars['String']>
+}
