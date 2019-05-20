60e689a20dae26014660c1054e32ca274b595143
Johnny Magrippis <j@magrippis.com>
2019-05-20 17:02:57 +0100

    upgrade dependencies

--- graphql/src/types.ts
+++ graphql/src/types.ts
@@ -1,4 +1,4 @@
-type Maybe<T> = T | null
+export type Maybe<T> = T | null
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string
@@ -16,6 +16,7 @@ export enum CacheControlScope {
 }
 
 export type Query = {
+  __typename?: 'Query'
   greeting: Scalars['String']
 }
 
