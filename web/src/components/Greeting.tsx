bc7cd86c4d7ac934ac794ddda364b50e7dafcef0
Johnny Magrippis <j@magrippis.com>
2019-06-20 14:29:03 +0100

    Use react-native-web for opinionated frontend

--- web/src/components/Greeting.tsx
+++ web/src/components/Greeting.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
+import { Text, View, StyleSheet } from 'react-native'
 
 import { Query as QueryType, QueryGreetingArgs } from '../types'
 
@@ -12,15 +13,25 @@ const GREETING_QUERY = gql`
 
 class GreetingQuery extends Query<QueryType, QueryGreetingArgs> {}
 
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1
+  }
+})
+
 export const Greeting = () => (
-  <GreetingQuery query={GREETING_QUERY}>
-    {({ loading, error, data }) => {
-      if (loading) return 'loading...'
-      if (error || !data) return 'error fetching greeting!'
+  <View style={styles.container}>
+    <GreetingQuery query={GREETING_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) return <Text>loading...</Text>
+        if (error || !data) return <Text>error fetching greeting!</Text>
 
-      const { greeting } = data
+        const { greeting } = data
 
-      return greeting
-    }}
-  </GreetingQuery>
+        return <Text>{greeting}</Text>
+      }}
+    </GreetingQuery>
+  </View>
 )
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,11 +1,12 @@
-import * as React from 'react'
-import ReactDOM from 'react-dom'
+import { AppRegistry } from 'react-native'
 import 'reset-css'
 
+import './index.css'
 import { App } from './components/App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+AppRegistry.registerComponent('App', () => App)
+AppRegistry.runApplication('App', { rootTag: document.getElementById('root') })
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
